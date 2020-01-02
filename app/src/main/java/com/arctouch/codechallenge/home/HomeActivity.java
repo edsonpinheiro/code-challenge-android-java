dec9cf400d4ffa24181de2af3566cc27bae878ba
edson <edson.bf.pinheiro@gmail.com>
2020-01-01 23:45:51 -0300

    Removing BaseActivity and old comments

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.java
@@ -3,58 +3,21 @@ package com.arctouch.codechallenge.home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
-import android.widget.ProgressBar;
 
 import com.arctouch.codechallenge.R;
-import com.arctouch.codechallenge.api.TmdbApi;
-import com.arctouch.codechallenge.base.BaseActivity;
-import com.arctouch.codechallenge.data.Cache;
-import com.arctouch.codechallenge.details.DetailsScreenFragment;
-import com.arctouch.codechallenge.model.Genre;
-import com.arctouch.codechallenge.model.Movie;
 
-import java.util.ArrayList;
-import java.util.Objects;
+public class HomeActivity extends AppCompatActivity {
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-
-public class HomeActivity extends BaseActivity {
-
-    private FrameLayout frameLayout;
-    private RecyclerView recyclerView;
-    private ProgressBar progressBar;
+//    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
-
-        this.frameLayout = findViewById(R.id.frameLayout);
-//        this.recyclerView = findViewById(R.id.recyclerView);
-//        this.progressBar = findViewById(R.id.progressBar);
-
+//        this.frameLayout = findViewById(R.id.frameLayout);
         openFragment(new HomeScreenFragment());
-
-//        api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1L, TmdbApi.DEFAULT_REGION)
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(response -> {
-//                    for (Movie movie : response.results) {
-//                        movie.genres = new ArrayList<>();
-//                        for (Genre genre : Cache.getGenres()) {
-//                            if (movie.genreIds.contains(genre.id)) {
-//                                movie.genres.add(genre);
-//                            }
-//                        }
-//                    }
-//
-//                    recyclerView.setAdapter(new HomeAdapter(response.results));
-//                    progressBar.setVisibility(View.GONE);
-//                });
     }
 
     public void openFragment(Fragment fragment) {
--- app/src/main/java/com/arctouch/codechallenge/splash/SplashActivity.java
+++ app/src/main/java/com/arctouch/codechallenge/splash/SplashActivity.java
@@ -3,10 +3,10 @@ package com.arctouch.codechallenge.splash;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import com.arctouch.codechallenge.R;
 import com.arctouch.codechallenge.api.TmdbApi;
-import com.arctouch.codechallenge.base.BaseActivity;
 import com.arctouch.codechallenge.data.Cache;
 import com.arctouch.codechallenge.home.HomeActivity;
 import com.arctouch.codechallenge.model.ApiModel;
@@ -14,7 +14,7 @@ import com.arctouch.codechallenge.model.ApiModel;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends AppCompatActivity {
     private ApiModel apiModel;
 
     @Override
